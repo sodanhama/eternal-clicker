c04f4c63abee22a591bb2b8d6ca584b156b6597f
Soham <brainosoham@gmail.com>
2026-08-18 16:14:50 +0530

    added clicker, now implementing clicking and more upgrades

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let balance = document.querySelector('.balance');
-let gems = 0
+let clickerCost = document.querySelector('.clicker-cost');
+
+let parsedBalance = parseFloat(balance.innerHTML);
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
 
 function incrementBalance() {
-    gems++;
-    balance.innerHTML = gems;
+    parsedBalance += 1;
+    balance.innerHTML = parsedBalance;
+}
+
+function buyClicker() {
+    if (parsedBalance >= parsedClickerCost) {
+        parsedBalance -= parsedClickerCost;
+        balance.innerHTML = parsedBalance;
+    }
 }
