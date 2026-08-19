8bc618f6f9ef6defa1e7e77bd29915cfccebbef8
Soham <brainosoham@gmail.com>
2026-08-19 08:44:04 +0530

    implemented boost to balance with upgrade, fixed math using parsefloat and rounding

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let balance = document.querySelector('.balance');
 let clickerCost = document.querySelector('.clicker-cost');
 
-let parsedBalance = parseFloat(balance.innerHTML);
+let clickerLevel = document.querySelector('.clicker-level');
+let clickerIncrease = document.querySelector('.clicker-increase');
+
 let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let parsedClickerLevel = parseFloat(clickerLevel.innerHTML);
+let parsedBalance = parseFloat(balance.innerHTML);
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML);
+
+let gpc = 1;
 
 function incrementBalance() {
-    parsedBalance += 1;
-    balance.innerHTML = parsedBalance;
+    balance.innerHTML = Math.round(parsedBalance += gpc);
 }
 
 function buyClicker() {
     if (parsedBalance >= parsedClickerCost) {
-        parsedBalance -= parsedClickerCost;
-        balance.innerHTML = parsedBalance;
+        balance.innerHTML = Math.round(parsedBalance -= parsedClickerCost);
+
+        clickerLevel.innerHTML++;
+
+        parsedClickerIncrease = parseFloat((parsedClickerIncrease * 1.03).toFixed(2));
+        clickerIncrease.innerHTML = parsedClickerIncrease
+
+        gpc += parsedClickerIncrease;
+
+        parsedClickerCost *= 1.18;
+        clickerCost.innerHTML = Math.round(parsedClickerCost)
     }
 }
